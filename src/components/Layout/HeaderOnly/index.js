3fa606e1d7e52b9e850c4a79f11944c394a281e0
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2022-05-09 14:24:24 +0700

    Config router + Layout

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../DefaultLayout/Header";
+
+export const HeaderOnly = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
+};
